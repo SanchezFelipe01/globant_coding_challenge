6b4b869ed22bb1f5f2ab95bae1d11b09b88bdd4a
Felipe Sánchez <vegafelipe015@gmail.com>
2024-12-29 19:27:56 -0500

    HTML templates modified

--- app/__main__.py
+++ app/__main__.py
@@ -11,6 +11,8 @@ db.init_app(app)
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 csv_departments_path = os.path.join(current_directory, '..', 'files', 'departments.csv')
+csv_jobs_path  = os.path.join(current_directory, '..', 'files', 'jobs.csv')
+csv_employees_path = os.path.join(current_directory, '..', 'files', 'hired_employees.csv')
 
 @app.route('/')
 def home():
@@ -33,6 +35,49 @@ def delete_all_departments():
     return departments_crud.delete_all_departments(db, Department)
 
 ### DEPARTMENTS ###
+
+### JOBS ###
+
+@app.route('/import_jobs_csv')
+def import_jobs_csv():
+    return jobs_crud.import_jobs_csv(db, Job, csv_jobs_path)
+
+
+@app.route('/get_all_jobs')
+def get_all_jobs():
+    return jobs_crud.get_all_jobs(Job)
+    
+@app.route('/delete_all_jobs')
+def delete_all_jobs():
+    return jobs_crud.delete_all_jobs(db, Job)
+
+### JOBS ###
+
+### EMPLOYEES ###
+
+@app.route('/import_employees_csv')
+def import_employees_csv():
+    return employees_crud.import_employees_csv(db, Employee, csv_employees_path)
+
+@app.route('/get_all_employees')
+def get_all_employees():
+    return employees_crud.get_all_employees(Employee)
+
+@app.route('/get_employees_missing_info')
+def get_employees_with_missing_info():
+    return employees_crud.get_employees_with_missing_info(Employee)
+
+@app.route('/delete_all_employees')
+def delete_all_employees():
+    return employees_crud.delete_all_employees(db, Employee)
+
+### EMPLOYEES ###
+
+### QUERIES ###
+
+
+
+### QUERIES ###
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
